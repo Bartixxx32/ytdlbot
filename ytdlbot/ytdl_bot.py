d2e47d8809cee04fdc2b5fe21ff7e4e2e2b05a32
Bartixxx32 <mcbplay1@gmail.com>
2023-02-01 23:07:00 +0100

    disable list

--- ytdlbot/ytdl_bot.py
+++ ytdlbot/ytdl_bot.py
@@ -331,10 +331,6 @@ def download_handler(client: "Client", message: "types.Message"):
         message.reply_text("I think you should send me a link.", quote=True)
         return
 
-    if re.findall(r"^https://www\.youtube\.com/channel/", VIP.extract_canonical_link(url)) or "list" in url:
-        message.reply_text("Channel/list download is disabled now. Please send me individual video link.", quote=True)
-        red.update_metrics("reject_channel")
-        return
     # non vip user, consume too many token
     if (not VIP().check_vip(chat_id)) and (not lim.consume(str(chat_id).encode(), 1)):
         red.update_metrics("rate_limit")
